29391af8b6dc290f1231d155441fa9a35e17e647
Lesliemwangi <luciannahphoi@gmail.com>
2024-07-31 09:24:03 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import MyNavbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
+import Signup from './components/Signup'
+import Login from './components/Login'
 import Footer from './components/Footer'
 
 function App() {
@@ -15,8 +17,10 @@ function App() {
 	  <MyNavbar />
       <Routes>
         <Route path="/" element={<Home />} />
-		<Route path="/about" element={<About />} />
+		    <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
     </Router>
